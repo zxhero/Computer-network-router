1344d16c5d3ee9aade8da1e0ced5ea08a943d0d9
Xu Zhang <zhangxu415@mails.ucas.ac.cn>
2018-05-04 08:21:00 +0800

    Fix length of icmp packet

--- icmp.c
+++ icmp.c
@@ -14,12 +14,12 @@ void icmp_send_packet(const char *in_pkt, int len, u8 type, u8 code)
 	fprintf(stderr, "TODO: malloc and send icmp packet.\n");
 	struct iphdr *ip_hdr = packet_to_ip_hdr(in_pkt);
 	u32 dst = ntohl(ip_hdr->daddr);
-	len = ETHER_HDR_SIZE + IP_BASE_HDR_SIZE + 16 + IP_BASE_HDR_SIZE;
 	struct icmphdr *icmp = (struct icmphdr *)IP_DATA(ip_hdr);
         if(type == ICMP_ECHOREPLY && code == 0){
             ;
         }
         else{
+            len = ETHER_HDR_SIZE + IP_BASE_HDR_SIZE + 16 + IP_BASE_HDR_SIZE;
             memcpy((char *)icmp+8,ip_hdr,IP_HDR_SIZE(ip_hdr)+8);
             memset((char *)icmp+4,0,4);
             //struct iphdr *ip_new = packet_to_ip_hdr(icmp_packet);
